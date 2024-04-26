3ae4d044eb3808671b1438ed666c081dec8a6fba
Yordi <yordiyachm@gmail.com>
2024-04-26 15:17:50 -0600

    First Implementation

--- bases/src/app/app.module.ts
+++ bases/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
