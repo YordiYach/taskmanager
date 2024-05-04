dcef55a93252abd12c553f89a536212001a2c645
Yordi <yordiyachm@gmail.com>
2024-05-04 10:09:07 -0600

    Sidebar Delete

--- bases/src/app/app.module.ts
+++ bases/src/app/app.module.ts
@@ -3,20 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { SignInComponent } from './components/sign-in/sign-in.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { HeaderComponent } from './components/header/header.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    SignInComponent,
-    DashboardComponent,
     HeaderComponent,
-    SidebarComponent,
   ],
   imports: [
     BrowserModule,
