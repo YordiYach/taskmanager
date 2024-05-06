00ac6083be698f7fbfc07c9ae6d1144ee0774df1
Wilson Chalí <wilsonchaliecc@gmail.com>
2024-05-05 20:45:55 -0600

    Progress bar

--- bases/src/app/app.module.ts
+++ bases/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    ProgressBarComponent
   ],
   imports: [
     BrowserModule,
