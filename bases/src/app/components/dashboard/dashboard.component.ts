92f959f007b801bc66d3e614f92b40ac95f386b2
Manuel Alexander <aja21791@uvg.edu.gt>
2024-05-11 10:32:21 -0600

    login and signIn design

--- bases/src/app/components/dashboard/dashboard.component.ts
+++ bases/src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-dashboard',
+  standalone: true,
+  imports: [HeaderComponent],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent {}
--- bases/src/app/components/sign-in/sign-in.component.ts
+++ bases/src/app/components/sign-in/sign-in.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from '../login/login.component';
+import { RouterModule } from '@angular/router';
+import { DashboardComponent } from '../dashboard/dashboard.component';
 
 @Component({
   selector: 'app-sign-in',
+  standalone: true,
+  imports: [LoginComponent, RouterModule, DashboardComponent],
   templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.css'
+  styleUrl: './sign-in.component.css',
 })
-export class SignInComponent {
-
-}
+export class SignInComponent {}
