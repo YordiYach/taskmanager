99b29a9010d2f96401e072d01da91e30958fca52
OurAleky <sayalex35@gmail.com>
2024-05-11 15:40:11 -0600

    SE TERMINO DE CONFIGURAR EL MODAL

--- bases/src/app/app-routing.module.ts
+++ bases/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HeaderComponent } from './components/header/header.component';
-
+import { TaskComponent } from './components/task/task.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'header', component: HeaderComponent },
+  { path: 'task', component: TaskComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' }, // redirect to `login` route by default
 ];
 
--- bases/src/app/app.module.ts
+++ bases/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LoginComponent } from './components/login/login.component';
+import { TaskComponent } from './components/task/task.component';
+import { TasknewComponent } from './components/task/tasknew/tasknew.component';
+import { FormsModule } from '@angular/forms'; 
+import { TaskService } from './components/task/task.service.ts';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, LoginComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  declarations: [AppComponent, HeaderComponent, LoginComponent, TaskComponent, TasknewComponent],
+  imports: [BrowserModule, AppRoutingModule,FormsModule],
+  providers: [TaskService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
