f431a1736bc140176b50821e88ca9a502e26726f
Yordi <yordiyachm@gmail.com>
2024-05-15 20:21:45 -0600

    Componente de Bienvenida

--- bases/src/app/components/dashboard/dashboard.component.ts
+++ bases/src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { ProgressBarComponent } from '../progress-bar/progress-bar.component';
+import { BienvenidaComponent } from '../bienvenida/bienvenida.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HeaderComponent, ProgressBarComponent],
+  imports: [HeaderComponent, ProgressBarComponent, BienvenidaComponent],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent {}
+export class DashboardComponent { }
